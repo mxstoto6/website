a9094c3a852b497aadf42497a159c1b7a4748516
mxstoto6 <mxstotolatomate@gmail.com>
2023-04-23 16:38:13 +0200

    Added the base for a router system

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,29 @@
+<template>
+  <div class="content">
+    <router-link to="/"> Home </router-link>
+    <router-link to="/guide"> Guide </router-link>
+    <router-view />
+  </div>
+</template>
+
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
-
-<style scoped>
+<style>
 * {
-  user-select: none; /*WHY ISNT THIS WORKING???*/
+  user-select: none;
+  
 }
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+
+a {
+  font-weight: bold;
+  color: #2c3e50;
+  text-decoration: none;
+  margin-right: 1em;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+a:hover, a.router-link-active {
+  border-bottom: 2px solid #3498db;
 }
 </style>
---
+++ src/components/Form.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <input type="text" placeholder="Name" v-model="name">
+    <div>
+      <button @click="submitForm" v-bind:disabled="name.length == 0">Submit</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { ref } from 'vue'
+
+  export default {
+    setup() {
+      const name = ref('')
+
+      const submitForm = () => {
+        console.log(`Form submitted! Name = ${name.value}`)
+      }
+
+      return {
+      name,
+      submitForm,
+      }
+    },
+  }
+</script>
+
+<style>
+  input[type='text'] {
+    padding: 5px;
+  }
+
+  button {
+    margin-top: 20px;
+  }
+</style>
