1e17fa1c8a43ac9868b03ecd2cff7c4cb7447fad
mxstoto6 <mxstotolatomate@gmail.com>
2023-04-25 20:10:23 +0200

    Added icons and improved tabs

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <template>
   <div class="content">
     <div class="header">
-      <router-link to="/"> Home </router-link>
-      <router-link to="/myWork"> My work </router-link>
-      <router-link to="/links"> Links </router-link>
-      <router-link to="/more"> More </router-link>
+
+      <router-link to="/">
+        <img src="./assets/icons8_home.ico" alt="(Home)">
+        <p>Home</p>
+      </router-link>
+
+      <router-link to="/myWork">
+        <img src="./assets/icons8_portfolio.ico" alt="(Home)">
+        <p>My work</p>
+      </router-link>
+
+      <router-link to="/links">
+        <img src="./assets/icons8_linking.ico" alt="(Home)">
+        <p>Links</p>
+      </router-link>
+
+      <router-link to="/more">
+        <img src="./assets/icons8_categorize.ico" alt="(Home)">
+        <p>More</p>
+      </router-link>
+
     </div>
     <router-view v-slot="{ Component, route }">
       <transition
@@ -34,18 +51,41 @@
     color: #ffffffaf;
     text-decoration: none;
     margin: 1em;
+    display: flex;
+    justify-content: center;
+    width: auto;
+    display: flex;
+    width: 7em;
+    transition: .2s ease-in-out;
+    border-bottom: 3.5px solid #393053;
+    border-radius: 3px;
+    padding-bottom: 5px;
+    -webkit-tap-highlight-color: transparent;
+    
   }
 
   a:hover, a.router-link-active {
-    border-bottom: 2px solid #635985;
+    border-bottom: 3.5px solid #635985;
   }
 
   .header {
-    padding: 2rem;
+    padding: 0.5rem;
     background-color: #393053;
     width: 100%;
     margin: 0 auto;
     text-align: start;
+    display: flex;
+    justify-content: center;
+  }
+
+  img {
+    opacity: 70%;
+    font-size: 18px;
+    margin-right: 10px;
+  }
+
+  .header p {
+    margin-top: 4px;
   }
 
 </style>
